06c139b6e541572648c097f2952f2f6a481e9f7e
JOAO_LEITE <joao.lsouza@agu.gov.br>
2024-01-22 11:13:05 -0300

    add<index> cors implemented

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 const models = require("./src/services/models/models");
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   res.send("connected to API!");
